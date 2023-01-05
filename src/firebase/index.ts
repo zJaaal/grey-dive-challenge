110ac6f71ef5d7278f2f6b8dedaabdfa70f05454
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-05 16:13:34 -0300

    firebase config

---
+++ src/firebase/index.ts
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, doc, getDoc, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+async function saveOnDatabase(data: any) {
+  try {
+    const docRef = await addDoc(collection(db, "responses"), data);
+    console.log("Document written with ID: ", docRef.id);
+    return docRef.id;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+async function getResponses(uid: string) {
+  try {
+    const data = await getDoc(doc(db, `responses/{uid}`));
+    console.log("Document written with ID: ", data);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+export { db, saveOnDatabase, getResponses };
