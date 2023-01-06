6261136e8255e9f567d67f35c13aadeb62ca73cb
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-06 20:42:55 -0300

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,18 @@
-import rawData from "./db.json";
+import jsonData from "./db.json";
 
-export const data: DataType = rawData.items;
+//Terms and conditions is the end of the form. So the one before is the last item
+const lastItemIndex = jsonData.items.findIndex((item) => item.name == "terms_and_conditions") - 1;
 
+//The last component with lastItem and checkboxes, not submit
+export const lastComponent: DataType = jsonData.items.slice(
+  lastItemIndex,
+  jsonData.items.length - 1
+);
+
+export const data: DataType = jsonData.items.slice(0, lastItemIndex);
+export const rawData: DataType = jsonData.items;
+
+export const submitData = rawData[rawData.length - 1];
 export type DataType = (InputType | SelectType)[];
 export type InputType = {
   name?: string;
