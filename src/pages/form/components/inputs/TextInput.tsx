2741657c69657798b402bf72957d388f24741fc7
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-05 22:51:25 -0300

    created dynamic TextInput
    - Added all types for inputs

--- src/pages/form/components/inputs/TextInput.tsx
+++ src/pages/form/components/inputs/TextInput.tsx
@@ -1,11 +1,28 @@
-import { FormControl, TextField } from "@mui/material";
+import { FormControl, FormHelperText, SxProps, TextField, Typography } from "@mui/material";
 import React, { RefObject } from "react";
 import { TextInputProps } from "./types";
 
 const TextInput = React.forwardRef<RefObject<HTMLInputElement>, TextInputProps>((props, ref) => {
+  let inputStyle: SxProps = {
+    height: "24px",
+    width: "50%",
+  };
+
+  let formStyle: SxProps = {
+    width: "100vw",
+    display: "flex",
+    alignContent: "center",
+    flexWrap: "wrap",
+    marginBottom: "40px",
+  };
+
   return (
-    <FormControl>
-      <TextField required inputRef={ref} variant={props.variant} />
+    <FormControl {...props} sx={formStyle}>
+      <Typography variant="h5" gutterBottom>
+        {props.label} <span style={{ color: "red" }}>*</span>
+      </Typography>
+      <TextField inputRef={ref} {...props} label="" sx={inputStyle} />
+      <FormHelperText></FormHelperText>
     </FormControl>
   );
 });
--- src/pages/form/components/inputs/types.ts
+++ src/pages/form/components/inputs/types.ts
@@ -1,3 +1,18 @@
-import { FormControlProps, TextFieldProps } from "@mui/material";
+import {
+  CheckboxProps,
+  FormControlLabelProps,
+  FormControlProps,
+  MenuItemProps,
+  SelectProps,
+  TextFieldProps,
+} from "@mui/material";
+import { DatePickerProps } from "@mui/x-date-pickers";
 
 export type TextInputProps = FormControlProps & TextFieldProps;
+export type DateInputProps = DatePickerProps<Date, Date> & FormControlProps;
+export type SelectInputProps = SelectProps & FormControlProps & { options: OptionsProps[] };
+export type CheckboxInputProps = CheckboxProps & FormControlProps & FormControlLabelProps;
+
+interface OptionsProps extends MenuItemProps {
+  label: string;
+}
