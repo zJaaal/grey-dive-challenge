643436428cdb310e9394d6e8e50d8073a3fe56fa
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-05 22:52:43 -0300

    create InputTypes to read dynamic inputs

---
+++ src/pages/form/utils/index.ts
@@ -0,0 +1,13 @@
+import CheckboxInput from "../components/inputs/CheckboxInput";
+import DateInput from "../components/inputs/DateInput";
+import SelectCustomInput from "../components/inputs/SelectInput";
+import TextInput from "../components/inputs/TextInput";
+
+export const InputTypes: any = {
+  text: TextInput,
+  email: TextInput,
+  date: DateInput,
+  select: SelectCustomInput,
+  checkbox: CheckboxInput,
+  submit: null,
+};
