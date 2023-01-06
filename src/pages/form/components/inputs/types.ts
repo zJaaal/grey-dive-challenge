85ebe85824812269e4df6fc73c91de4186267975
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-06 14:32:02 -0300

    Update types.ts

--- src/pages/form/components/inputs/types.ts
+++ src/pages/form/components/inputs/types.ts
@@ -7,9 +7,11 @@ import {
   TextFieldProps,
 } from "@mui/material";
 import { DatePickerProps } from "@mui/x-date-pickers";
+import { Moment } from "moment";
+import { ValidateResponse } from "../../../../context/validation/types";
 
 export type TextInputProps = FormControlProps & TextFieldProps;
-export type DateInputProps = DatePickerProps<Date, Date> & FormControlProps;
+export type DateInputProps = DatePickerProps<Date, Moment> & FormControlProps & TextFieldProps;
 export type SelectInputProps = SelectProps & FormControlProps & { options: OptionsProps[] };
 export type CheckboxInputProps = CheckboxProps & FormControlProps & FormControlLabelProps;
 
