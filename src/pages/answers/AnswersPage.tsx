19737df47866ded68430a155a8bc5717449f1e73
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-08 14:29:52 -0300

    Update AnswersPage.tsx

--- src/pages/answers/AnswersPage.tsx
+++ src/pages/answers/AnswersPage.tsx
@@ -1,7 +1,12 @@
 import { Grid, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
+import { getResponses } from "../../firebase";
 
 const AnswersPage = () => {
+  useEffect(() => {
+    getResponses().then((result) => console.log(result));
+  }, []);
+
   return (
     <Grid
       sx={{
