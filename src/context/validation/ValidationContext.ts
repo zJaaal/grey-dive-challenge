3982f7120e639c956a965976b1979bbd28694c31
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-09 20:17:34 -0300

    add type to context

--- src/context/validation/ValidationContext.ts
+++ src/context/validation/ValidationContext.ts
@@ -1,3 +1,12 @@
 import { createContext } from "react";
 
-export const ValidationContext = createContext<any>({});
+type ValidationContextType = {
+  formValues?: any;
+  handleFormValueChange?: (key: string, value: string | Date | boolean) => void;
+  saveAnswers?: () => Promise<boolean>;
+  validateFormValue?: (key: string, value?: string | Date | boolean) => void;
+  formErrors?: any;
+  loading?: boolean;
+};
+
+export const ValidationContext = createContext<ValidationContextType>({});
