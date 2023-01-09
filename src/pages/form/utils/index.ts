47b972892bb1b7ff3acda3fde5f94b4d0d91a7a9
Jalinson Diaz <zjaaaldev@gmail.com>
2023-01-09 20:08:13 -0300

    create getProgressBarTransition

--- src/pages/form/utils/index.ts
+++ src/pages/form/utils/index.ts
@@ -1,3 +1,4 @@
+import { Variants } from "framer-motion";
 import CheckboxInput from "../components/inputs/CheckboxInput";
 import DateInput from "../components/inputs/DateInput";
 import SelectCustomInput from "../components/inputs/SelectInput";
@@ -12,4 +13,18 @@ export const InputComponents: any = {
   select: SelectCustomInput,
   checkbox: CheckboxInput,
   submit: SubmitInput,
+};
+
+export const getProgressBarTransition = (prev: number, value: number, max: number): Variants => {
+  let currentPercentage = (value * 100) / max;
+  let lastPercentage = (prev * 100) / max;
+
+  return {
+    in: {
+      background: `linear-gradient(to right, transparent 0%, transparent ${currentPercentage}%, white ${currentPercentage}%, white 100%)`,
+    },
+    out: {
+      background: `linear-gradient(to right, transparent 0%, transparent ${lastPercentage}%, white ${lastPercentage}%, white 100%)`,
+    },
+  };
 };
